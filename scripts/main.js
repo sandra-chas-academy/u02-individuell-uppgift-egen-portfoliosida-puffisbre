4748325d6d3ff512545b7f2d24fef647ebfc480d
FelixOS <puffis.stenersjo@gmail.com>
2024-11-25 00:38:04 +0100

    added click effect to tree logo

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,28 @@ const aboutImage = document.querySelector('.about-image');
 const homeText = document.querySelector('.home-text');
 const aboutText = document.querySelector('.about-text');
 const welcomeContainer = document.querySelector('.welcome-container');
+const profileImageContainer = document.querySelector('.profile-image-container');
+const profileImage = document.querySelector('.profile-image');
+const headerContainer = document.querySelector('.header-container');
+
+let treeClicked = false;
+
+profileImageContainer.addEventListener('click', () =>{
+ if(treeClicked === false){
+  profileImage.style.filter = 'invert(17%) sepia(28%) saturate(627%) hue-rotate(85deg) brightness(99%) contrast(89%))';
+  setTimeout(() => {
+    profileImageContainer.style.transform = 'scale(1)';
+  }, 100);
+  treeClicked = true;
+ }else if(treeClicked === true){
+  profileImageContainer.style.transform = 'scale(0.5)';
+  setTimeout(() => {
+    profileImageContainer.style.transform = 'scale(1)';
+  }, 100);
+  profileImage.style.filter = 'invert(17%) sepia(28%) saturate(627%) hue-rotate(85deg) brightness(99%) contrast(89%)';
+  treeClicked = false;
+ }
+})
 
 
 burgerMenu.addEventListener('click', () =>{
